19779a1ca851ff155081eb0cfc272dacb89c049e
Gringo Gang <miroslav.pecek1@gmail.com>
2023-04-07 11:26:59 +0200

    add other cycle

--- calculadorys.py
+++ calculadorys.py
@@ -22,3 +22,26 @@ else:
     print("Invalid operator")
 
 
+input1 = input("Write first number: ")
+input2 = input("Choose the operator: + , - , * , / , // , %: ")
+input3 = input("Write second number: ")
+
+number1 = float(input1)
+operator = str(input2)
+number2 = float(input3)
+
+if operator == "+":
+    print(number1+number2)
+elif operator == "-":
+    print(number1-number2)
+elif operator == "*":
+    print(number1*number2)
+elif operator == "/":
+    print(number1/number2)
+elif operator == "//":
+    print(number1//number2)
+elif operator == "%":
+    print(number1%number2)
+else:
+    print("Invalid operator")
+
